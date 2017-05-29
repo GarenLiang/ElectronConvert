ba06893852f726c9f93952505ae3999fafc162c9
GarenLiang <ideabreeze@gmail.com>
2017-05-28 18:01:11 -0700

    Multi Videos Finish

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
-
+const _ = require('lodash');
 const { app, BrowserWindow, ipcMain } = electron;
 
 let mainWindow;
@@ -16,10 +16,18 @@ app.on('ready', () => {
 });
 
 ipcMain.on('videos:added', (event, videos) => {
-  const promise = new Promise((resolve, reject) => {
-    ffmpeg.ffprobe(videos[0].path, (err, metadata) => {
-      resolve(metadata);
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(videos[0].path, (err, metadata) => {
+        video.duration = metadata.format.duration;
+        video.format = 'avi';
+        resolve(video);
+      });
     });
   });
-  promise.then((metadata) => { console.log(metadata); })
+
+  Promise.all(promises)
+    .then((results) => {
+      mainWindow.webContents.send('metadata:complete', results);
+    });
 });
